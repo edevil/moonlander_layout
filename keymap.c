aaf17c36f10bd263f43299b5e9f0c2fdcdafb6fb
André Cruz <acruz@cloudflare.com>
2020-12-28 16:57:25 +0000

    Added shortcut to move between tabs.

--- keymap.c
+++ keymap.c
@@ -33,9 +33,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
     KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_LEFT,                                        KC_RIGHT,       KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           PTM_QUOT,
     KC_DELETE,      KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TG(1),                                          TG(1),          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
-    KC_BSPACE,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_HYPR,                                                                        KC_MEH,         KC_H,           KC_J,           KC_K,           KC_L,           LT(2,PTM_CCED),  LGUI_T(PTM_TILD),
+    KC_TAB,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_HYPR,                                                                        KC_MEH,         KC_H,           KC_J,           KC_K,           KC_L,           LT(2,PTM_CCED),  LGUI_T(PTM_TILD),
     PTM_LSPO,        LCTL_T(KC_Z),   KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         RCTL_T(PTM_MINS),PTM_RSPC,
-    LT(1,PTM_LABK),  ST_MACRO_0,     LALT(KC_LSHIFT),KC_LEFT,        KC_RIGHT,       LALT_T(KC_SLASH),                                                                                                LCTL_T(KC_ESCAPE),KC_UP,          KC_DOWN,        PTM_PLUS,        PTM_ACUT,        MO(1),
+    LT(1,PTM_LABK),  ST_MACRO_0,     LGUI(KC_LALT),  KC_LEFT,        KC_RIGHT,       PTM_SLSH,                                                                                                        LCTL_T(KC_ESCAPE),KC_UP,          KC_DOWN,        PTM_PLUS,        PTM_ACUT,        MO(1),
     KC_SPACE,       KC_BSPACE,      KC_LGUI,                        KC_LALT,        KC_TAB,         KC_ENTER
   ),
   [1] = LAYOUT_moonlander(
