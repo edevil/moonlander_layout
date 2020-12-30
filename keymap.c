83950e6bbe3df76a70e9e99fdafec64905e9e936
André Cruz <acruz@cloudflare.com>
2020-12-30 16:16:43 +0000

    Corrected macro

--- keymap.c
+++ keymap.c
@@ -107,7 +107,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case ST_MACRO_0:
     if (record->event.pressed) {
-      SEND_STRING(SS_LCMD(SS_TAP(X_GRAVE)));
+      SEND_STRING(SS_LCMD(SS_TAP(X_NUBS)));
     }
     break;
     case PT_LSPO:
